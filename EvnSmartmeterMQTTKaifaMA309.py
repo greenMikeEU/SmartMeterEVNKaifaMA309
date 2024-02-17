013318efd9d40cd3a54eb477489845b38d332547
greenMike <mreitbauer@gmx.net>
2024-02-17 22:59:42 +0100

    Update EvnSmartmeterMQTTKaifaMA309.py
    Parser geändert

--- EvnSmartmeterMQTTKaifaMA309.py
+++ EvnSmartmeterMQTTKaifaMA309.py
@@ -62,7 +62,7 @@ while 1:
         pdu.clear()
         xml += tr.messageToXml(msg)
 
-    soup = BeautifulSoup(xml, 'lxml')
+    soup = BeautifulSoup(xml, 'html5lib')
 
     results_32 = soup.find_all('uint32')
     results_16 = soup.find_all('uint16')
